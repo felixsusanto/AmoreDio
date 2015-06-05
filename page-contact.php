94b092587e39590535883cd703591cff4112f5da
evantage <evantage@gmail.com>
2015-06-05 12:41:57 +0800

    contact-page updated to use hash

--- page-contact.php
+++ page-contact.php
@@ -18,6 +18,28 @@ get_header(); ?>
 
             <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
+            <!-- added auto load certain tabs based on the anchor url -->
+            <script>
+              var contact_y = 300;
+              var url = document.location.toString();
+              if (url.match('#')) {
+                  $(window).scroll(contact_y); // go up to show Contact nicely
+                  document.body.scrollTop=contact_y;
+                  $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+              }
+
+              // Change hash for page-reload
+              $('.nav-tabs a').on('shown.bs.tab', function (e) {
+                  e.preventDefault();
+                  if(history.pushState) {
+                    history.pushState(null, null, e.target.hash);
+                  }
+                  else {
+                    window.location.hash = '#myhash';
+                  }
+              })
+            </script>
+
           <?php endwhile; // end of the loop. ?>
         </div>
         <!-- /.span -->
