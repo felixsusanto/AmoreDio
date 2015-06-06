cc2fd28babf00bbad22999e0e4ef0abe984374c4
evantage <evantage@gmail.com>
2015-06-06 11:10:09 +0800

    fixed the offset and pagination issue. redirect to main page if over paging

--- page-event.php
+++ page-event.php
@@ -20,11 +20,22 @@ get_header(); ?>
 
           <?php endwhile; // end of the loop. ?>
           <!-- table of content to the songbook list -->
+
+          <?php 
+            $highlight_post_id = array();
+            $args_event = array('post_type'=>'event', 'showposts'=> 1);
+            $event = new WP_Query($args_event);
+            if ($event) {
+              foreach ($event as $post) {
+                $highlight_post_id[] = $post->ID;
+              }
+            }
+          ?>
+          
+          <?php if (get_query_var('paged') <= 1) : ?>
           <div class="upcoming-event">
             <h2 class="text-center text-red"><i class="fa fa-bullhorn"></i> Upcoming Event</h2>
             <?php
-              $args_event = array('post_type'=>'event', 'showposts'=> 1);
-              $event = new WP_Query($args_event);
               if ($event->have_posts()): while($event->have_posts()): $event->the_post();
             ?>
               <?php if(get_field('poster_image')): ?>
@@ -62,17 +73,20 @@ get_header(); ?>
                 </div><!-- /.span -->
               </div><!-- /.row -->
             <?php endwhile; endif; wp_reset_query(); ?>  
+          
           </div><!-- /.upcoming-event -->
+          <?php endif; ?>
+
+
           <div class="previous-event">
             <h2 class="text-center text-red"><i class="fa fa-bullhorn"></i> Previous Event Test</h2>
             <?php
               // First, initialize how many posts to render per page
-              $display_count = 1;
+              $display_count = 2;
               // Next, get the current page
               $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-              // After that, calculate the offset
-              $offset = ( $page - 1 ) * $display_count;
-              $args_pastevent = array('post_type'=>'event', 'offset'=>$offset, 'posts_per_page'=> $display_count, 'paged'=> $paged );
+              // use post not in to exclude the highlighted post
+              $args_pastevent = array('post_type'=>'event', 'post__not_in'=>$highlight_post_id, 'posts_per_page'=> $display_count, 'paged'=> $paged );
               $pastevent = new WP_Query($args_pastevent);
               if($pastevent->have_posts()): while($pastevent->have_posts()): $pastevent->the_post();
             ?>
@@ -109,6 +123,13 @@ get_header(); ?>
             </div><!-- /.past-event-module -->
               
             <?php endwhile; wp_pagenavi(array('query'=>$pastevent)); ?>
+
+            <?php elseif ($paged > 1): // redirect to main event page if overshoot the pagination / empty event?>
+              <script>
+                var str = window.location.href;
+                str = str.substr(0, str.lastIndexOf("page/"));
+                window.location = str;
+              </script>
 
             <?php endif; wp_reset_query(); ?>
           </div> <!-- /.previous-event -->
