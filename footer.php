82e582308ff86301d536223c9a16f334546a5408
Felix Susanto <felix.susanto2000@gmail.com>
2018-09-02 21:01:45 +0800

    editing position of soc media links

--- footer.php
+++ footer.php
@@ -15,11 +15,18 @@
       <div class="row">
         <div class="col-md-3">
           <p>2014 &copy; copyrights AmoreDio</p>
-          <p>Follow us on: 
-            <a href="https://www.facebook.com/cellgroup.amoredio/" target="_blank"><i class="fa fa-facebook-square fa-lg"></i> Facebook</a>
-            <a href="https://www.instagram.com/amoredio.sg/?hl=en" target="_blank"><i class="fa fa-instagram fa-lg"></i> Instagram</a>
-            <a href="https://www.youtube.com/channel/UC7w5l6jOzUeWadRA9jjEUVQ" target="_blank"><i class="fa fa-youtube-play fa-lg"></i> Youtube</a>
-          </p>
+          <p>Follow us on: </p>
+          <ul class="list-unstyled">
+            <li>
+              <a href="https://www.facebook.com/cellgroup.amoredio/" target="_blank"><i class="fa fa-facebook-square fa-lg"></i> Facebook</a>              
+            </li>
+            <li>
+              <a href="https://www.instagram.com/amoredio.sg/?hl=en" target="_blank"><i class="fa fa-instagram fa-lg"></i> Instagram</a>              
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UC7w5l6jOzUeWadRA9jjEUVQ" target="_blank"><i class="fa fa-youtube-play fa-lg"></i> Youtube</a>              
+            </li>
+          </ul>
         </div><!-- /.span -->
         <div class="col-md-9">
           <h3>Site Map</h3>
